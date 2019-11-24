3118b9f5825df3a565aaccd089f4cc2b45c4e34a
alokbharti <bhartialok.257@gmail.com>
2019-11-24 21:14:15 +0530

    code to display automatic modified product list
    - Adapter files are removed instead FirebaseRecyclerAdapter is used by
    using FirebaseUI
    - XML file added to display user particular donated product

--- app/src/main/java/com/example/donationbox/ui/ngo/ProductClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.donationbox.ui.ngo;
-
-import com.example.donationbox.ui.donate.Donor;
-
-public interface ProductClickListener {
-
-    void onClaimListener(Donor donor, int position);
-    void onUndoClaimListener(Donor donor, int position);
-}
